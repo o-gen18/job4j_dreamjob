90a8876b9e451a6353ba8568f98de29f3b5d8769
Ol Gen <oggen18@gmail.com>
2020-12-18 18:15:27 +0300

    First commit

---
+++ src/main/java/ru/job4j/Recruiter.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class Recruiter {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
