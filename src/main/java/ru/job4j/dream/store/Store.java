155eb314bfb1faa5974c93caf5357ed877cb914c
Ol Gen <oggen18@gmail.com>
2021-01-06 16:33:10 +0300

    made maps as final fields

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -16,8 +16,8 @@ public class Store {
     private static AtomicInteger POST_ID = new AtomicInteger(4);
     private static AtomicInteger CANDIDATE_ID = new AtomicInteger(4);
 
-    private Map<Integer, Post> posts = new ConcurrentHashMap<>();
-    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job"));
