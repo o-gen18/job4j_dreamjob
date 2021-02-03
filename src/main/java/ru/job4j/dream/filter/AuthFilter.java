f6f62f1f100201ebc80d371e8abfa9c8eb1aa190
Ol Gen <oggen18@gmail.com>
2021-02-03 18:13:15 +0300

    Adding authorisation filter.

---
+++ src/main/java/ru/job4j/dream/filter/AuthFilter.java
@@ -0,0 +1,34 @@
+package ru.job4j.dream.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest sReq, ServletResponse sResp, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) sReq;
+        HttpServletResponse resp = (HttpServletResponse) sResp;
+        String uri = req.getRequestURI();
+        if (uri.endsWith("auth.do")) {
+            chain.doFilter(sReq, sResp);
+            return;
+        }
+        if (req.getSession().getAttribute("user") == null) {
+            resp.sendRedirect(req.getContextPath() + "/login.jsp");
+            return;
+        }
+        chain.doFilter(sReq, sResp);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
